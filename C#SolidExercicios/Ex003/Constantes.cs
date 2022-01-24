570d77ffc60c7caf62e0a2539904e44b882de339
Caetano Menin Geteins Vidal <desenvolvimento2+caetano@injepro.com>
2022-01-24 11:20:57 -0300

    respostas resolvidas professor

---
+++ C#SolidExercicios/Ex003/Constantes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex003
+{
+    public static class Constantes
+    {
+        public const decimal IMPOSTO_USA = 0.3m;
+        public const decimal IMPOSTO_BR = 0.6m;
+        public const decimal IMPOSTO_URUGUAI = 0.25m;
+    }
+}
---
+++ C#SolidExercicios/Ex003/ImpostoBrasil.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex003
+{
+    class ImpostoBrasil : IImpostos
+    {
+        public decimal BaseImposto(decimal amount)
+        {
+            return amount - (amount * Constantes.IMPOSTO_BR);
+        }
+    }
+}
