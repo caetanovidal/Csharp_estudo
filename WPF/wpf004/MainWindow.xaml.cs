ca06fcacc5e9457cffd0043552a8a252736440f9
Caetano Menin Geteins Vidal <desenvolvimento2+caetano@injepro.com>
2022-02-01 17:49:48 -0300

    Secao09 finalizada- Rest and AI

---
+++ WPF/wpf004/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Win32;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using wpf004.Classes;
+
+namespace wpf004
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Image files (*.png; *.jpg; *.jpeg)|*.png; *.jpg; *.jpeg|All files(*.*)|*.*";
+            //dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            dialog.InitialDirectory = @"C:\Users\Windows\Downloads\Resources-REST-and-AI-Training-the-AI\WPF-1040-TrainAI-resources";
+
+            if (dialog.ShowDialog()== true)
+            {
+                string fileName = dialog.FileName;
+                selectedImage.Source = new BitmapImage(new Uri(fileName));
+
+                MakePredicitionAsync(fileName);
+            }
+        }
+
+        private async void MakePredicitionAsync(string fileName)
+        {
+            string url = "https://southcentralus.api.cognitive.microsoft.com/customvision/v3.0/Prediction/42f7f7da-2870-435e-ab93-04f673b5d4ce/classify/iterations/Iteration1/image";
+            string predicitionKey = "4984e6ce37c340e386dc1333822fee3c";
+            string contentType = "application/octet-stream";
+            var file = File.ReadAllBytes(fileName);
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Prediction-Key", predicitionKey);
+                
+                using(var content = new ByteArrayContent(file))
+                {
+                    content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(contentType);
+                    var response = await client.PostAsync(url, content);
+
+                    var responseString = await response.Content.ReadAsStringAsync();
+
+                    List<Prediction> predictions = (JsonConvert.DeserializeObject<CustomVision>(responseString)).Predictions;
+
+                    PredicionsListView.ItemsSource = predictions;
+                }
+            }
+        }
+    }
+}
